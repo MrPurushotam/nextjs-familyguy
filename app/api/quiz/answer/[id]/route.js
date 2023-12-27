638c413426f08c276bafeaf8504a7b8d6e023986
Purushotam <purupurushotamjeswani2004@gmail.com>
2023-12-28 04:49:54 +0530

    First Commit of FamilyGuy

---
+++ app/api/quiz/answer/[id]/route.js
@@ -0,0 +1,31 @@
+/**
+ * Retrieves a question and generates a random question for the quiz.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} params - The route parameters.
+ * @param {string} params.id - The ID of the question to retrieve.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing the correct answer and a randomly generated question ID, or an error response.
+ */
+
+import questions from '@/data/quiz.json'
+import { NextResponse } from 'next/server'
+
+export async function GET(req,{params}) {
+  try{
+    const question = questions.data.map(item=>item.id===params.id)
+    if(!question){
+      return new NextResponse('not found',{status:400})
+    }
+    const {correct_answer}=question
+    const filteredQuestions=questions.data.filter(item=>item.id!==params.id)
+    const random=Math.floor(Math.random()* filteredQuestions.length)
+    return NextResponse.json({
+      correct:correct_answer,
+      random:filteredQuestions[random].id,
+    })
+  }catch(err){
+    console.log(err.message)
+    return new NextResponse('internal server error',{status:500})
+  }
+}
